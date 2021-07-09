703ba1019fd66f44da67e6d20f15921e1e23a33f
Pavel Grebeniuk <pavel.g@2muchcoffee.com>
2021-07-09 15:54:18 +0300

    Add appointment types

---
+++ src/shared/types/appointment.ts
@@ -0,0 +1,30 @@
+export interface Appointment {
+  id: string;
+  startDate: string;
+  endDate: string;
+  clinicianName: string;
+  patient: Patient;
+  status: AppointmentStatus;
+}
+
+export interface Patient {
+  id: string;
+  name: string;
+}
+
+export enum AppointmentStatus {
+  active = 'ACTIVE',
+  canceled = 'CANCELLED'
+}
+
+export interface RawAppointment {
+  clinicianName: string;
+  patientName: string;
+  startDate: Date;
+  endDate: Date;
+}
+
+export enum GroupType {
+  clinician= 'CLINICIAN',
+  day = 'DAY'
+}
