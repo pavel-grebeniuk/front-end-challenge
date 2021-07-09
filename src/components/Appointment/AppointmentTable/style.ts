7b1ad7a87c0a8cc035751f5f30a9a1f5fcc7724b
Pavel Grebeniuk <pavel.g@2muchcoffee.com>
2021-07-09 15:55:31 +0300

    Create AppointmentTable component

---
+++ src/components/Appointment/AppointmentTable/style.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+  highlightedRow: {
+    backgroundColor: '#cfd2d2',
+  },
+  centerBox: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    marginTop: 200,
+  }
+});
